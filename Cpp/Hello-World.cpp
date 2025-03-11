46177a59acb926ccf092f2d9947f77c009fc018d
Manish Kumar Patel <maneeshkurmii@gmail.com>
2025-03-11 13:58:54 +0530

    starting cpp programming from scratch

---
+++ Cpp/Hello-World.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    const string userName = "Manish";
+    cout << "Hello World" << userName << "'s program of learning C++.";
+    return 0;
+}
