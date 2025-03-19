53cc51d7cc03bdffa8fa15ed88d7dd62e565cde1
Manish Kumar Patel <maneeshkurmii@gmail.com>
2025-03-19 10:18:28 +0530

    SOME MORE PRACTICE

--- Java/Placement Prep/Leetcode121MaxProfit.java
+++ Java/Placement Prep/Leetcode121MaxProfit.java
@@ -11,3 +11,34 @@ Input: prices = [7,6,4,3,1]
 Output: 0
 Explanation: In this case, no transactions are done and the max profit = 0.
  */
+
+public class Leetcode121MaxProfit {
+
+    static int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int sell = prices[0];
+        int profit = 0;
+        int i,j;
+        int size = prices.length;
+        for(i=1; i<size; i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            }
+        }
+        for(j=1; j<size; j++) {
+            if(prices[j] > sell) {
+                if(sell > buy) {
+                    profit = sell-buy;
+                }
+            }
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        int[] pricesList = {7,1,5,3,6,4};
+        int result = maxProfit(pricesList);
+        System.out.println(result);
+        
+    }
+}
